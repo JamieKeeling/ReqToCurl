efb56a60c6a0a8c96fa830620c429068c5cd0136
Jamie Keeling <jamie.keeling@hotmail.co.uk>
2018-12-24 23:33:34 +0000

    Driving package version via tagging

--- deploy/pushToNuget.sh
+++ deploy/pushToNuget.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
+dotnet pack ${TRAVIS_BUILD_DIR}/src/ReqToCurl/ReqToCurl.csproj -c Release -o ${TRAVIS_BUILD_DIR}/artifacts --no-build -p:PackageVersion=${TRAVIS_TAG}
 dotnet nuget push ${TRAVIS_BUILD_DIR}/artifacts/ReqToCurl.*.nupkg --api-key ${NUGET_API_KEY} -s https://api.nuget.org/v3/index.json
