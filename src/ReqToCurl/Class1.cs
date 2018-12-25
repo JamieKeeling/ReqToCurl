f58e07a9cd5733f237b453a0a55f227b9fb52559
Jamie Keeling <JamieKeeling@users.noreply.github.com>
2018-12-25 18:50:16 +0000

    Basic middleware with tests (#4)

--- src/ReqToCurl/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ReqToCurl
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/ReqToCurl/RequestToCurlMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ReqToCurl
+{
+    public static class RequestToCurlMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestToCurl(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<RequestToCurlMiddleware>();
+        }
+    }
+}
