239afb34b397fe30712acc963dd5192dc9493f80
Jamie Keeling <jamie.keeling@hotmail.co.uk>
2018-12-25 18:25:57 +0000

    Basic middleware with tests

---
+++ src/ReqToCurl/RequestToCurlMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ReqToCurl
+{
+    public class RequestToCurlMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestToCurlMiddleware> _logger;
+
+        public RequestToCurlMiddleware(RequestDelegate next, ILogger<RequestToCurlMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+
+            if (context.Response.HasStarted)
+            {
+                //TODO: Verify response status code matches options, otherwise ignore
+                _logger.Log(LogLevel.Information, $"Response has started with a HTTP {context.Response.StatusCode} value");
+            }
+        }
+    }
+}   
---
+++ test/ReqToCurl.Tests/InvocationTests.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Internal;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReqToCurl.Tests
+{
+    public class InvocationTests
+    {
+        [Fact]
+        public async void Invoked_WhenResponseHasStarted_IsTrue()
+        {
+            var mockLogger = new Mock<ILogger<RequestToCurlMiddleware>>();
+
+            var middlewareInstance = new RequestToCurlMiddleware((innerHttpContext) => Task.FromResult(0), mockLogger.Object);
+
+            var mockHttpResponse = new Mock<HttpResponse>();
+            mockHttpResponse.SetupGet(p => p.HasStarted).Returns(true);
+            mockHttpResponse.SetupGet(p => p.StatusCode).Returns(200);
+
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.SetupGet(p => p.Response).Returns(mockHttpResponse.Object);
+
+            await middlewareInstance.InvokeAsync(mockHttpContext.Object);
+
+            mockLogger.Verify(x => x.Log(LogLevel.Information,
+                                            It.IsAny<EventId>(),
+                                            new FormattedLogValues("Response has started with a HTTP 200 value"),
+                                            null,
+                                            It.IsAny<Func<object, Exception, string>>()),
+                                            Times.Once);
+        }
+
+        [Fact]
+        public async void NotInvoked_WhenResponseHasStarted_IsFalse()
+        {
+            var mockLogger = new Mock<ILogger<RequestToCurlMiddleware>>();
+
+            var middlewareInstance = new RequestToCurlMiddleware((innerHttpContext) => Task.FromResult(0), mockLogger.Object);
+
+            var mockHttpResponse = new Mock<HttpResponse>();
+            mockHttpResponse.SetupGet(p => p.HasStarted).Returns(false);
+
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.SetupGet(p => p.Response).Returns(mockHttpResponse.Object);
+
+            await middlewareInstance.InvokeAsync(mockHttpContext.Object);
+
+            mockLogger.Verify(x => x.Log(LogLevel.Information,
+                                            It.IsAny<EventId>(),
+                                            It.IsAny<FormattedLogValues>(),
+                                            null,
+                                            It.IsAny<Func<object, Exception, string>>()),
+                                            Times.Never);
+        }
+    }
+}
