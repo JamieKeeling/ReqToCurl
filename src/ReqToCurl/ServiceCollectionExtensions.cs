fa31c085d2a8e79f7fd4aa81f1a6128694e98728
Jamie Keeling <jamie.keeling@experian.com>
2019-01-13 19:38:58 +0000

    WIP

---
+++ src/ReqToCurl/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ReqToCurl
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddReqToCurl(this IServiceCollection services)
+        {
+            //TODO: Configure DI here
+            return services;
+        }
+    }
+}
---
+++ test/ReqToCurl.Tests/String Output/UrlTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ReqToCurl.Tests.String_Output
+{
+    public class UrlTests
+    {
+        [Fact]
+        public void UrlBuilder_WithHttpContext_OutputsRequestedUrl()
+        {
+
+        }
+    }
+}
